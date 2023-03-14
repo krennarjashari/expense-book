2c7072ddce32c6ec566b4f0d4ca463224acb06ae
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-03-14 13:34:16 +0100

    code format

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
       id: "e1",
       title: "Toilet Paper",
       amount: 94.12,
-      date: new Date(2023, 3, 14),
+      date: new Date(2023, 2, 14),
     },
     {
       id: "e2",
       title: "New TV",
       amount: 799.49,
-      date: new Date(2023, 3, 14),
+      date: new Date(2023, 2, 14),
     },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2023, 3, 14),
+      date: new Date(2023, 2, 14),
     },
   ];
 
@@ -29,12 +29,12 @@ function App() {
         amount={expenses[0].amount}
         date={expenses[0].date}
       />
-       <ExpenseItem
+      <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
       />
-       <ExpenseItem
+      <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
