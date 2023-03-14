a69697bce9ee71197092f3d4fbe17989efb953b2
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-03-14 11:18:51 +0100

    creating expenses array data for props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2023, 3, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2023, 3, 14),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2023, 3, 14),
+    },
+  ];
+
   return (
     <div>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+       <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+       <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
